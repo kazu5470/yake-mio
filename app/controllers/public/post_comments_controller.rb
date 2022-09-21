d460184476dbc081cb3b6c9d4657e85cd48f7a66
kazu5470 <rad.kazu08060806@gmail.com>
2022-09-21 06:54:21 +0000

    非同期完了

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,22 +2,19 @@ class Public::PostCommentsController < ApplicationController
     
     def create
         @post = Post.find(params[:post_id])
-        @post_comment = current_user.post_comments.new(post_comment_params)
-        @post_comment.post_id = @post.id
-        if @post_comment.save
-            @post = Post.find(params[:post_id])
-            @post_comment = PostComment.new
+        @comment = current_user.post_comments.new(post_comment_params)
+        @comment.post_id = @post.id
+        if @comment.save
+            redirect_to public_post_path(@post)
         else
-            render 'public/posts/show'
+            redirect_to public_post_path(@post)
             flash[:notice] = "コメントを入力してください"
         end    
     end
     
     def destroy
         PostComment.find(params[:id]).destroy
-        #redirect_to public_post_path(params[:post_id])
-        @post_comment = PostComment.find_by(id: params[:id], post_id: params[:post_id])
-        render :destroy
+        redirect_to public_post_path(params[:post_id])
     end
     
     
